aec5765095c6e5e9bde66f08bcf6dd5089479880
NamGung Hyemin <ng040115@gmail.com>
2024-03-24 20:52:14 +0900

    Initialize commit

---
+++ hwA202214052/src/test/java/net/skhu/HwA202214052ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwA202214052ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
