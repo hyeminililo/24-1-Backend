ee9a62fa9be63e6fc44867e39f6f4788883b0ab9
NamGung Hyemin <ng040115@gmail.com>
2024-03-27 00:50:58 +0900

    Implement mybatis_professor project using DB and STS

---
+++ mybatis_professor/src/main/java/net/skhu/mapper/ProfessorMapper.java
@@ -0,0 +1,45 @@
+package net.skhu.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import net.skhu.dto.Professor;
+@Mapper	
+public interface ProfessorMapper {
+	// 목록
+	@Select("SELECT * FROM professor WHERE id=#{id}")
+	Professor findOne(int id);
+	
+	@Select("""
+			SELECT p.*, d.name departmentName
+			FROM professor p LEFT JOIN department d ON p.departmentId = d.id
+			""")
+	List<Professor> findAll();
+	// 등록
+	@Insert("""
+			INSERT professor (name, departmentId, phone, email, office)
+			VALUES (#{name}, #{departmentId}, #{phone}, #{email}, #{office})
+			""")
+	@Options(useGeneratedKeys = true, keyProperty = "id")
+	void insert(Professor professor);
+	// 수정
+
+	@Update("""
+			UPDATE professor SET
+			name=#{name},
+			departmentId=#{departmentId},
+			phone=#{phone},
+			email=#{email},
+			office=#{office}
+			WHERE id = #{id} """)
+	void update(Professor professor);
+	// 삭제 -> id와 그런 공백도 주의해야함 ,, 
+	@Delete("DELETE FROM professor WHERE id =#{id}")
+	void delete(int id);
+}
